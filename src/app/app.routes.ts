23fcf1723dbb83d1ea751dadf33c09d5e8113da9
Ridmike <ridmikew@gmail.com>
2024-07-27 00:15:38 +0530

    side nav color

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {path:'',redirectTo:'/home',pathMatch:"full"},
   {path:'ads',loadComponent:()=>import('./components/ads/ads.component').then(c=>c.AdsComponent)},
-  // {path:'register',loadComponent:()=>import('./components/register/register.component').then(c=>c.RegisterComponent)},
+  {path:'register',loadComponent:()=>import('./components/register/register.component').then(c=>c.RegisterComponent)},
   {path:'cat',loadComponent:()=>import('./components/post-ad-cat/post-ad-cat.component').then(c=>c.PostAdCatComponent)},
   {path:'form',loadComponent:()=>import('./components/post-ad-form/post-ad-form.component').then(c=>c.PostAdFormComponent)},
   {path:'login',loadComponent:()=>import('./components/login/login.component').then(c=>c.LoginComponent)},
