22c8cf093ef0de74c0f5e1e6b4be4887a21304e5
Ishan <dakshinaishan88@gmail.com>
2024-08-10 20:35:36 +0530

    float button changed

--- src/app/components/main-outer/main-outer.component.ts
+++ src/app/components/main-outer/main-outer.component.ts
@@ -4,6 +4,7 @@ import {AdsPanelComponent} from "../ads-panel/ads-panel.component";
 import {FooterComponent} from "../footer/footer.component";
 import {FloatBtnComponent} from "../float-btn/float-btn.component";
 import {MatIcon} from "@angular/material/icon";
+import {NgStyle} from "@angular/common";
 
 @Component({
   selector: 'app-main-outer',
@@ -13,7 +14,8 @@ import {MatIcon} from "@angular/material/icon";
     AdsPanelComponent,
     FooterComponent,
     FloatBtnComponent,
-    MatIcon
+    MatIcon,
+    NgStyle
   ],
   templateUrl: './main-outer.component.html',
   styleUrl: './main-outer.component.css'
