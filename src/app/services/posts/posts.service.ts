d3c5ebd047f91781270f48655c317b6ce9c7261b
Ridmike <ridmikew@gmail.com>
2024-08-14 00:33:03 +0530

    main outer responsive

--- src/app/services/posts/posts.service.ts
+++ src/app/services/posts/posts.service.ts
@@ -20,17 +20,17 @@ export class PostsService{
       description: "We are seeking an Administrative Assistant to manage office tasks, schedule meetings, and support executives.",
       shortDescription: "Administrative Assistant role in NY office.",
       price: 50000,
-      category: { subId: 105, subCatName: "Administrative Assistant" },
+      category: { subId: 105, subCatName: "Bicycle" },
       img: "/icons/car.png",              //----------------this is home category img-----------------------
       section: {
         id: 1,
-        section: "Administrative",
+        section: "Vehicle",
         subCats: [
-          {subId: 101, subCatName: "Receptionist"},
-          {subId: 102, subCatName: "Office Clerk"},
-          {subId: 103, subCatName: "Data Entry Clerk"},
-          {subId: 104, subCatName: "Executive Assistant"},
-          {subId: 105, subCatName: "Administrative Assistant"}
+          { subId: 101, subCatName: "Car" },
+          { subId: 102, subCatName: "Van" },
+          { subId: 103, subCatName: "Motorbike" },
+          { subId: 104, subCatName: "Truck" },
+          { subId: 105, subCatName: "Bicycle" }
         ],
         img: ''               //----------------this is home category img auto add-----------------------
       },
@@ -38,22 +38,22 @@ export class PostsService{
     },
     {
       id: 2,
-      title: "Certified Nurse",
+      title: "Certified Electronic",
       location: "Los Angeles, CA",
       description: "Looking for a Certified Nurse to join our healthcare team. Responsibilities include patient care and assisting doctors.",
       shortDescription: "Certified Nurse position available in LA.",
       price: 75000,
-      category: { subId: 201, subCatName: "Nurse" },
+      category: { subId: 201, subCatName: "Mobile Phones" },
       img: "/icons/car.png",            //----------------this is home category img-----------------------
       section: {
         id: 2,
-        section: "Healthcare",
+        section: "Electronic",
         subCats: [
-          {subId: 201, subCatName: "Nurse"},
-          {subId: 202, subCatName: "Medical Assistant"},
-          {subId: 203, subCatName: "Pharmacy Technician"},
-          {subId: 204, subCatName: "Lab Technician"},
-          {subId: 205, subCatName: "Dental Assistant"}
+          { subId: 201, subCatName: "Mobile Phones" },
+          { subId: 202, subCatName: "Computers" },
+          { subId: 203, subCatName: "Audio Equipments" },
+          { subId: 204, subCatName: "Home Appliances" },
+          { subId: 205, subCatName: "Televisions" }
         ],
         img: ''           //----------------this is home category img-----------------------
       },
@@ -61,22 +61,22 @@ export class PostsService{
     },
     {
       id: 3,
-      title: "Software Developer",
+      title: "Property Developer",
       location: "San Francisco, CA",
       description: "Seeking a skilled Software Developer to create and maintain software applications for our tech firm.",
       shortDescription: "Software Developer needed in SF.",
       price: 90000,
-      category: { subId: 301, subCatName: "Software Developer" },
+      category: { subId: 301, subCatName: "Residential Properties" },
       img: "/icons/car.png",               //----------------this is home category img-----------------------
       section: {
         id: 3,
-        section: "IT & Technology",
+        section: "Property",
         subCats: [
-          {subId: 301, subCatName: "Software Developer"},
-          {subId: 302, subCatName: "System Analyst"},
-          {subId: 303, subCatName: "IT Support Specialist"},
-          {subId: 304, subCatName: "Network Administrator"},
-          {subId: 305, subCatName: "Database Administrator"}
+          { subId: 301, subCatName: "Residential Properties" },
+          { subId: 302, subCatName: "Land" },
+          { subId: 303, subCatName: "Recreational Properties" },
+          { subId: 304, subCatName: "Commercial Properties" },
+          { subId: 305, subCatName: "Agricultural Properties" }
         ],
         img: ''              //----------------this is home category img-----------------------
       },
@@ -84,22 +84,22 @@ export class PostsService{
     },
     {
       id: 4,
-      title: "Call Center Representative",
+      title: "Education Center Representative",
       location: "Chicago, IL",
       description: "We are hiring Call Center Representatives to handle customer inquiries and provide support.",
       shortDescription: "Call Center Representative job in Chicago.",
       price: 40000,
-      category: { subId: 401, subCatName: "Call Center Representative" },
+      category: { subId: 401, subCatName: "Online Education" },
       img: "/icons/car.png",                   //----------------this is home category img-----------------------
       section: {
         id: 4,
-        section: "Customer Service",
+        section: "Education",
         subCats: [
-          {subId: 401, subCatName: "Call Center Representative"},
-          {subId: 402, subCatName: "Customer Support Specialist"},
-          {subId: 403, subCatName: "Technical Support Representative"},
-          {subId: 404, subCatName: "Client Service Coordinator"},
-          {subId: 405, subCatName: "Help Desk Technician"}
+          { subId: 401, subCatName: "Online Education" },
+          { subId: 402, subCatName: "Vocational and Technical Education" },
+          { subId: 403, subCatName: "Arts Education" },
+          { subId: 404, subCatName: "Homeschooling" },
+          { subId: 405, subCatName: "Primary Education" }
         ],
         img: ''                        //----------------this is home category img-----------------------
       },
@@ -107,22 +107,22 @@ export class PostsService{
     },
     {
       id: 5,
-      title: "Financial Analyst",
+      title: "Grocery Analyst",
       location: "Boston, MA",
       description: "Financial Analyst needed to analyze financial data, prepare reports, and assist in decision-making.",
       shortDescription: "Financial Analyst opportunity in Boston.",
       price: 80000,
-      category: { subId: 502, subCatName: "Financial Analyst" },
+      category: { subId: 502, subCatName: "Dairy Products" },
       img: "/icons/car.png",             //----------------this is home category img-----------------------
       section: {
         id: 5,
-        section: "Finance",
+        section: "Grocery",
         subCats: [
-          {subId: 501, subCatName: "Accountant"},
-          {subId: 502, subCatName: "Financial Analyst"},
-          {subId: 503, subCatName: "Bookkeeper"},
-          {subId: 504, subCatName: "Payroll Specialist"},
-          {subId: 505, subCatName: "Auditor"}
+          { subId: 501, subCatName: "Bakery Items" },
+          { subId: 502, subCatName: "Dairy Products" },
+          { subId: 503, subCatName: "Pantry Staples" },
+          { subId: 504, subCatName: "Personal Care" },
+          { subId: 505, subCatName: "Household Essentials" }
         ],
         img: ''              //----------------this is home category img-----------------------
       },
@@ -130,22 +130,22 @@ export class PostsService{
     },
     {
       id: 6,
-      title: "HR Coordinator",
+      title: "Caregiving Coordinator",
       location: "Austin, TX",
       description: "HR Coordinator needed to manage HR tasks, employee relations, and recruitment processes.",
       shortDescription: "HR Coordinator role in Austin.",
       price: 60000,
-      category: { subId: 601, subCatName: "HR Coordinator" },
+      category: { subId: 601, subCatName: "Elderly Care" },
       img: "/icons/car.png",                 //----------------this is home category img-----------------------
       section: {
         id: 6,
-        section: "Human Resources",
+        section: "Caregiving",
         subCats: [
-          {subId: 601, subCatName: "HR Coordinator"},
-          {subId: 602, subCatName: "Recruiter"},
-          {subId: 603, subCatName: "HR Assistant"},
-          {subId: 604, subCatName: "Benefits Administrator"},
-          {subId: 605, subCatName: "Training Coordinator"}
+          { subId: 601, subCatName: "Elderly Care" },
+          { subId: 602, subCatName: "Child Care" },
+          { subId: 603, subCatName: "Disability Care" },
+          { subId: 604, subCatName: "Mental Health Support" },
+          { subId: 605, subCatName: "Specialized Care" }
         ],
         img: ''                    //----------------this is home category img-----------------------
       },
@@ -162,13 +162,13 @@ export class PostsService{
       img: "/icons/car.png",                 //----------------this is home category img-----------------------
       section: {
         id: 7,
-        section: "Education",
+        section: "Workers",
         subCats: [
-          {subId: 701, subCatName: "Substitute Teacher"},
-          {subId: 702, subCatName: "Teacher Assistant"},
-          {subId: 703, subCatName: "Tutor"},
-          {subId: 704, subCatName: "School Counselor"},
-          {subId: 705, subCatName: "Librarian"}
+          { subId: 701, subCatName: "Substitute Teacher" },
+          { subId: 702, subCatName: "Plumber" },
+          { subId: 703, subCatName: "Tutor" },
+          { subId: 704, subCatName: "School Counselor" },
+          { subId: 705, subCatName: "Electrician" }
         ],
         img: ''            //----------------this is home category img-----------------------
       },
@@ -176,22 +176,22 @@ export class PostsService{
     },
     {
       id: 8,
-      title: "Quality Control Inspector",
+      title: "Transport Inspector",
       location: "Detroit, MI",
       description: "Seeking a Quality Control Inspector to ensure manufacturing standards are met in our production facility.",
       shortDescription: "Quality Control Inspector needed in Detroit.",
       price: 55000,
-      category: { subId: 803, subCatName: "Quality Control Inspector" },
+      category: { subId: 803, subCatName: "Emergency and Critical Transport" },
       img: "/icons/car.png",                  //----------------this is home category img-----------------------
       section: {
         id: 8,
-        section: "Manufacturing",
+        section: "Transport",
         subCats: [
-          {subId: 801, subCatName: "Assembler"},
-          {subId: 802, subCatName: "Machinist"},
-          {subId: 803, subCatName: "Quality Control Inspector"},
-          {subId: 804, subCatName: "Production Worker"},
-          {subId: 805, subCatName: "Packaging Operator"}
+          { subId: 801, subCatName: "Logistics and Freight" },
+          { subId: 802, subCatName: "Tourist and Leisure Transport" },
+          { subId: 803, subCatName: "Emergency and Critical Transport" },
+          { subId: 804, subCatName: "Sustainable Transport" },
+          { subId: 805, subCatName: "Car Rentals" }
         ],
         img: ''                  //----------------this is home category img-----------------------
       },
@@ -199,22 +199,22 @@ export class PostsService{
     },
     {
       id: 9,
-      title: "Event Staff",
+      title: "Beauty Care Staff",
       location: "Las Vegas, NV",
       description: "Hiring Event Staff to assist with organizing and managing events. Responsibilities include setup, coordination, and customer service.",
       shortDescription: "Event Staff required for events in Las Vegas.",
       price: 35000,
-      category: { subId: 902, subCatName: "Event Staff" },
+      category: { subId: 902, subCatName: "Hair Care" },
       img: "/icons/car.png",             //----------------this is home category img-----------------------
       section: {
         id: 9,
-        section: "Hospitality",
+        section: "Beauty Care",
         subCats: [
-          {subId: 901, subCatName: "Housekeeper"},
-          {subId: 902, subCatName: "Event Staff"},
-          {subId: 903, subCatName: "Catering Assistant"},
-          {subId: 904, subCatName: "Front Desk Agent"},
-          {subId: 905, subCatName: "Concierge"}
+          { subId: 901, subCatName: "Skin Care" },
+          { subId: 902, subCatName: "Hair Care" },
+          { subId: 903, subCatName: "Makeup" },
+          { subId: 904, subCatName: "Fragrances" },
+          { subId: 905, subCatName: "Men’s Grooming" }
         ],
         img: ''             //----------------this is home category img-----------------------
       },
@@ -222,22 +222,22 @@ export class PostsService{
     },
     {
       id: 10,
-      title: "Warehouse Worker",
+      title: "Emergency Worker",
       location: "Phoenix, AZ",
       description: "Warehouse Workers needed to manage inventory, load/unload trucks, and maintain warehouse organization.",
       shortDescription: "Warehouse Worker positions in Phoenix.",
       price: 40000,
-      category: { subId: 1101, subCatName: "Warehouse Worker" },
+      category: { subId: 1101, subCatName: "Travel Emergencies" },
       img: "/icons/car.png",               //----------------this is home category img-----------------------
       section: {
         id: 11,
-        section: "Logistics",
+        section: "Emergency",
         subCats: [
-          {subId: 1101, subCatName: "Warehouse Worker"},
-          {subId: 1102, subCatName: "Delivery Driver"},
-          {subId: 1103, subCatName: "Forklift Operator"},
-          {subId: 1104, subCatName: "Shipping Clerk"},
-          {subId: 1105, subCatName: "Inventory Specialist"}
+          { subId: 1101, subCatName: "Travel Emergencies" },
+          { subId: 1102, subCatName: "Ambulance Services" },
+          { subId: 1103, subCatName: "Home Security Systems" },
+          { subId: 1104, subCatName: "Urgent Care" },
+          { subId: 1105, subCatName: "Water Supply Issues" }
         ],
         img: ''            //----------------this is home category img-----------------------
       },
