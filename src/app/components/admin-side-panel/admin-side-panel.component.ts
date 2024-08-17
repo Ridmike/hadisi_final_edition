072cbd87a4bec95d59a6ca5c6cd135f3c8b9234b
Ishan <dakshinaishan88@gmail.com>
2024-08-18 03:56:06 +0530

    admin changed

--- src/app/components/admin-side-panel/admin-side-panel.component.ts
+++ src/app/components/admin-side-panel/admin-side-panel.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import {NgForOf} from "@angular/common";
+import {MatIcon} from "@angular/material/icon";
 
 @Component({
   selector: 'app-admin-side-panel',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf,
+    MatIcon
+  ],
   templateUrl: './admin-side-panel.component.html',
   styleUrl: './admin-side-panel.component.css'
 })
 export class AdminSidePanelComponent {
+
 
 }
